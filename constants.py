a424eb6dc9987660e740f0972d466d980b3d6898
Simohamed0 <mohamed.faid@etu.unistra.fr>
2023-07-31 13:30:08 +0200

    progress

---
+++ constants.py
@@ -0,0 +1,18 @@
+# constants for the maze game
+
+# Maze constants
+MAZE_WIDTH = 10
+MAZE_HEIGHT = 10
+MAZE_WALL = 1
+MAZE_PATH = 0
+MAZE_START = 2
+MAZE_EXIT = 3
+MAZE_AGENT = 4
+
+# Agent constants
+AGENT_DUMB = 0
+AGENT_SMART = 1
+AGENT_RANDOM = 2
+AGENT_HUMAN = 3
+AGENT_AI = 4
+
--- maze/maze.py
+++ maze/maze.py
@@ -1,22 +1,53 @@
 import numpy as np
 import random
 
+MAZE_WALL = 1
+MAZE_PATH = 0
+MAZE_START = 3
+MAZE_EXIT = 4
+
+
 class Maze:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.matrix = np.zeros((width, height), dtype=int)
-        self.start = random.randint(0, width - 1), 0
-        self.exit = random.randint(0, width - 1), height - 1
+        self.matrix = np.full((width, height), MAZE_WALL, dtype=int)
+        self.start = 0,1
+        self.exit = width - 1, height - 2
         self.agent_position = self.start
         self.agent_path = [self.agent_position]
         self.difficulty = 1
         self.generate_random_maze()  # Optionally, you can generate random mazes here.
 
-    # function to generate random maze
     def generate_random_maze(self):
-        # using rando
-        pass
+        def get_neighbors(cell):
+            x, y = cell
+            neighbors = [(x+dx, y+dy) for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]]
+            random.shuffle(neighbors)
+            return [neighbor for neighbor in neighbors if 0 <= neighbor[0] < self.width and 0 <= neighbor[1] < self.height]
+
+        def dfs(cell):
+            self.matrix[cell] = MAZE_WALL
+
+            for neighbor in get_neighbors(cell):
+                x, y = neighbor
+                if self.matrix[x, y] == MAZE_WALL:
+                    nx, ny = (x + cell[0]) // 2, (y + cell[1]) // 2
+                    self.matrix[nx, ny] = MAZE_PATH
+                    dfs(neighbor)
+
+        dfs(self.start)
+
+        
+        # Restore the walls on the perimeter of the maze
+        self.matrix[:, 0] = MAZE_WALL
+        self.matrix[:, -1] = MAZE_WALL
+        self.matrix[0, :] = MAZE_WALL
+        self.matrix[-1, :] = MAZE_WALL
+
+        # Set the start and exit cells
+        self.matrix[self.start] = MAZE_START
+        self.matrix[self.exit] = MAZE_EXIT
 
 
     def is_wall(self, x, y):
