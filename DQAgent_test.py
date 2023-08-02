3e8780f0c580690983e78b2567d6e99e6e5bd14b
Simohamed0 <mohamed.faid@etu.unistra.fr>
2023-08-02 17:27:50 +0200

    [DEBUG]

--- DQAgent_test.py
+++ DQAgent_test.py
@@ -4,22 +4,26 @@ import numpy as np
 from agent.DQ_agent import DQAgent
 
 
-def train_agent(agent, maze, num_episodes=1000, max_steps_per_episode=1000):
+def train_agent(agent, num_episodes=1000, max_steps_per_episode=1000):
     for episode in range(1, num_episodes + 1):
-        state = np.reshape(state, [1, agent.state_size])  # Reshape state for DQN input
+        state = agent.get_state(agent.maze.matrix, agent.maze.exit)
+        agent.state_size = len(state)
         total_reward = 0
 
+
+        
         for step in range(max_steps_per_episode):
             # Take an action using the DQAgent's epsilon-greedy policy
-            action = agent.act(state)
+            action = agent.act()
 
             # Perform the action in the environment
-            next_state, reward, done = maze.step(action)
-            next_state = np.reshape(next_state, [1, agent.state_size])  # Reshape next state for DQN input
+            next_state, reward, done = agent.step(action)
+
 
             # Store the experience in the replay buffer
             agent.remember(state, action, reward, next_state, done)
-
+            
+            
             # Move to the next state
             state = next_state
 
@@ -28,6 +32,7 @@ def train_agent(agent, maze, num_episodes=1000, max_steps_per_episode=1000):
 
             if done:
                 break
+
 
         # Perform experience replay to update the DQNetwork
         agent.replay()
@@ -42,13 +47,29 @@ def train_agent(agent, maze, num_episodes=1000, max_steps_per_episode=1000):
 
 if __name__ == "__main__":
 
-    maze = Maze(width=c.MAZE_WIDTH, height= c.MAZE_HEIGHT)
+    maze = Maze(width= c.MAZE_WIDTH, height= c.MAZE_HEIGHT)
     print(maze.matrix)
 
     # Create the DQAgent
     dqn_agent = DQAgent(maze, action_size=4,
                         learning_rate=0.001, discount_factor=0.99, epsilon=1.0, epsilon_decay=0.995,
                         epsilon_min=0.01, batch_size=32, memory_size=1000)
+    
+    for i in range(100):
+        state = dqn_agent.get_state(maze.matrix, maze.exit)
+        a = dqn_agent.act()
+        print(a)
+        print("before", dqn_agent.position)
+        dqn_agent.step(a)
+        print("after", dqn_agent.position)
+        next_state = dqn_agent.get_state(maze.matrix, maze.exit)
+        print(np.array_equal(state, next_state))
 
-    # Train the agent
-    train_agent(dqn_agent, maze, num_episodes=1000)
+        dqn_agent.remember(state, a, 0, next_state, False)
+
+    # let's see what's inside memory
+    for i in range(5):
+        print(dqn_agent.memory[0][i])
+
+    # debug the replay function
+    dqn_agent.replay()
--- agent/DQ_agent.py
+++ agent/DQ_agent.py
@@ -1,17 +1,29 @@
 import numpy as np
 import random
 from collections import deque
+from agent.agent import Agent
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # '2' means to only display error messages
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 
 
+# actions
+UP = 0
+DOWN = 1
+LEFT = 2
+RIGHT = 3
 
-class DQAgent:
+
+
+class DQAgent(Agent):
     def __init__(self, maze, action_size=4, learning_rate=0.001, discount_factor=0.99,
-                 epsilon=1.0, epsilon_decay=0.995, epsilon_min=0.01, batch_size=32, memory_size=1000):
+                 epsilon=0, epsilon_decay=0.995, epsilon_min=0.01, batch_size=32, memory_size=1000, tau=0.1):
         
-        self.state_size = maze.width * maze.height
+        super().__init__(maze)
+        self.state_size = 0
         self.action_size = action_size
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
@@ -19,10 +31,18 @@ class DQAgent:
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
         self.batch_size = batch_size
+        self.tau = tau
         self.memory = deque(maxlen=memory_size)
 
         # Build the Deep Q-Network
         self.model = self._build_model()
+        self.target_model = self._build_model()
+        self._soft_update_target_network()
+
+    
+
+    def reset_position(self):
+        self.position = self.maze.starting_position
 
     def _build_model(self):
         model = Sequential()
@@ -42,18 +62,78 @@ class DQAgent:
 
         return model
 
+    # remember(state, action, reward, next_state, done) function
     def remember(self, state, action, reward, next_state, done):
+        # Add the experience to agent's memory
         self.memory.append((state, action, reward, next_state, done))
+        
 
-    def act(self, state):
+    
+    def get_state(self,maze_matrix, goal_position):
+        state = []
+
+        # Add agent's x and y coordinates to the state
+        state.extend(self.position)
+
+        # Flatten the maze matrix and add it to the state
+        for row in maze_matrix:
+            state.extend(row)
+
+        # Add goal's x and y coordinates to the state
+        state.extend(goal_position)
+
+        # Calculate and add the Manhattan distance from the agent to the goal
+        distance_to_goal = abs(self.position[0] - goal_position[0]) + abs(self.position[1] - goal_position[1])
+        state.append(distance_to_goal)
+
+        return state
+
+
+    def act(self):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
-        return np.argmax(self.model.predict(state)[0])
+        return np.argmax(self.model.predict(self.state)[0])
+
+    def step(self, action):
+        # Perform the given action in the environment (maze)
+
+        # Update the agent's position based on the chosen action
+        if action == UP:
+            next_position = (self.position[0] - 1, self.position[1])
+        elif action == DOWN:
+            next_position = (self.position[0] + 1, self.position[1])
+        elif action == LEFT:
+            next_position = (self.position[0], self.position[1] - 1)
+        elif action == RIGHT:
+            next_position = (self.position[0], self.position[1] + 1)
+        
+
+        # Check if the next position is within the maze boundaries
+        if self.is_valid_position(next_position):
+            self.position = next_position
+
+        # Get the state representation at the new position
+        next_state = self.get_state(self.maze.matrix, self.maze.exit)
+
+        # Check if the agent has reached the goal position
+        done = self.position == self.maze.exit
+
+        # Assign a reward based on the agent's new position
+        if done:
+            reward = 0
+        elif self.maze.matrix[self.position] == 1:
+            reward = -100
+            done = True
+        else:
+            reward = -1
+            
+        return next_state, reward, done
 
     def replay(self):
         # Check if the replay buffer has enough experiences to sample a batch
         if len(self.memory) < self.batch_size:
             return
+
 
         # Sample a batch of experiences from the replay buffer
         minibatch = np.array(random.sample(self.memory, self.batch_size))
